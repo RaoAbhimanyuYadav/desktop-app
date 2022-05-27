abc1bba2039b9ad821b2b2fe4ac77295c44373d4
RaoAbhimanyuYadav <raoyadavabhi@gmail.com>
2022-05-27 22:22:54 +0530

    filter dropdown remaining

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Filter from "./components/Filter";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Filter />
     </div>
   );
 }
